0d2ed72b55bbe9d272edd97fcd342602b4af8d6b
Arbabhassan786 <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-27 16:15:28 +0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,16 +6,21 @@ const Navbar = ({ currentPage }) => {
   return (
     <>
       <div className="grid grid-cols-5 border-b-2 border-black py-4 px-2 fixed w-full bg-white z-50">
-        {currentPage === "signup" ? (
-          <Link to="/" className="flex items-center">
-            <IoMdArrowRoundBack />
-          </Link>
-        ) : (
-          <div>
-            <IoMdArrowRoundBack />
-          </div>
-        )}
-        <h1 className="justify-self-center col-span-3	text-2xl font-bold">Condocare</h1>
+        <div className="self-center">
+          {currentPage === "signup" && (
+            <Link to="/" className="flex items-center">
+              <IoMdArrowRoundBack size={20} />
+            </Link>
+          )}
+          {currentPage === "settings" && (
+            <Link to="/signup" className="flex items-center">
+              <IoMdArrowRoundBack size={20}/>
+            </Link>
+          )}
+        </div>
+        <h1 className="justify-self-center col-span-3	text-2xl font-bold">
+          Condocare
+        </h1>
       </div>
     </>
   );
