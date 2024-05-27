e40ba90b84cf8f6acbee3eff93630711db480fff
Arbabhassan786 <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-27 17:52:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Signin from "./pages/Signin/Signin";
 import Signup from "./pages/Signup/Signup";
 import Settings from "./pages/Settings/Settings";
+import AddTask from "./pages/AddTask/AddTask";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="/add-task" element={<AddTask />} />
           {/* 
           
           <Route path="/merchandise" element={<Merchandise />} /> */}
