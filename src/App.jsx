d64b2cc41c013020dafe77e09bf40bf345bfdb77
Arbabhassan786 <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-27 16:17:47 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Signin from "./pages/Signin/Signin";
 import Signup from "./pages/Signup/Signup";
+import Settings from "./pages/Settings/Settings";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/settings" element={<Settings />} />
           {/* 
-          <Route path="/the-magazine" element={<TheMagazine />} />
+          
           <Route path="/merchandise" element={<Merchandise />} /> */}
         </Routes>
         {/* <Footer /> */}
