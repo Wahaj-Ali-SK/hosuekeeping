ee826b8771917a92c1db77f8acfe8848f65251ee
Arbabhassan786 <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-27 17:51:47 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { IoHomeSharp } from "react-icons/io5";
 import { FaPlusCircle } from "react-icons/fa";
 import { MdDateRange } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="flex justify-around fixed bottom-0 w-full bg-white z-50 px-2 py-4 border-t-2 border-black">
       <IoHomeSharp size={20}/>
-      <FaPlusCircle size={20}/>
+      <Link to="/add-task"><FaPlusCircle size={20}/></Link>
       <MdDateRange size={20}/>
     </div>
   );
