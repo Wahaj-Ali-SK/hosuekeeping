788ff06221ba15355b094ecc980a29c49f1c3527
Arbabhassan786 <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-28 16:30:06 +0500

    Update Signup.jsx
    Update the design

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -7,29 +7,32 @@ const Signup = () => {
     <>
       <Navbar currentPage="signup" />
       <div className="flex flex-col py-2 px-8 absolute mt-16">
-        <h1 className="text-left font-bold text-lg">Sign Up</h1>
+        <h1 className="text-center font-bold text-lg">Sign Up</h1>
+        <p className="text-center">Please enter the required information</p>
         <form action="submit" className="mt-2">
           <input
             type="text"
-            className="border-2 border-black h-12 px-2 mb-2 w-full"
+            className="border-gray-400 border-2 rounded-md bg-input-bg h-12 px-2 mb-2 w-full"
             placeholder="Name"
           />
           <input
             type="email"
-            className="border-2 border-black h-12 px-2 mb-2 w-full"
+            className="border-gray-400 border-2 rounded-md bg-input-bg h-12 px-2 mb-2 w-full"
             placeholder="Email"
           />
           <input
-            className="border-2 border-black h-12 px-2 mb-2 w-full"
+            className="border-gray-400 border-2 rounded-md bg-input-bg h-12 px-2 mb-2 w-full"
             type="password"
             id="pasword"
             name="pasword"
             placeholder="Password"
           />
-          <button className="bg-black text-white p-3 w-full mt-2">Sign Up</button>
+          <button className="bg-blue-700 text-white p-3 rounded-md w-full mt-2">Sign Up</button>
         </form>
 
-        <Link to="/" className="text-center w-full mt-4">Sign In</Link>
+        <span className="text-center w-full mt-4 font-[400] text-gray-500">
+          Already have an account? <Link to="/" className="text-blue-500 font-[600]">Sign In</Link>
+        </span>
       </div>
     </>
   );
