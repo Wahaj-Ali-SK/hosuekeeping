2d9f91af2b6e7b45d783c95317f37ea2e28f3a2d
Arbabhassan786 <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-28 17:13:18 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="flex justify-around fixed bottom-0 w-full bg-button z-50 px-2 py-4 border-t-2 border-black text-[#ffe4c4]">
-      <Link to='/'><IoHomeSharp size={20}/></Link>
-      <Link to="/add-task"><FaPlusCircle size={20}/></Link>
-      <MdDateRange size={20}/>
+    <div className="flex justify-around items-center fixed bottom-0 w-full bg-button z-50 px-2 py-4 border-t-2 border-black text-white">
+      <Link to='/'><IoHomeSharp size={22}/></Link>
+      <Link to="/add-task"><FaPlusCircle size={28}/></Link>
+      <MdDateRange size={22}/>
     </div>
   );
 };
