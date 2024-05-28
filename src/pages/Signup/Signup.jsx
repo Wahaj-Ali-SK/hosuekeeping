47f8cca12993d59b3702d3f4696c3f097723e745
Arbabhassan786 <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-28 16:40:52 +0500

    Update Signup.jsx
    Add confirm password input field.

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword((prevState) => !prevState);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
     <>
       <Navbar currentPage="signup" />
@@ -20,18 +33,58 @@ const Signup = () => {
             className="border-gray-400 border-2 rounded-md bg-input-bg h-12 px-2 mb-2 w-full"
             placeholder="Email"
           />
-          <input
-            className="border-gray-400 border-2 rounded-md bg-input-bg h-12 px-2 mb-2 w-full"
-            type="password"
-            id="pasword"
-            name="pasword"
-            placeholder="Password"
-          />
-          <button className="bg-blue-700 text-white p-3 rounded-md w-full mt-2">Sign Up</button>
+          
+          <div className="flex border-gray-400 border-2 items-center justify-between bg-input-bg rounded-md">
+            <input
+              className="h-12 w-full bg-input-bg px-2 rounded-md"
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+            {showPassword ? (
+              <FaEyeSlash
+                onClick={togglePasswordVisibility}
+                className="absolute right-12"
+              />
+            ) : (
+              <FaEye
+                onClick={togglePasswordVisibility}
+                className="absolute right-12"
+              />
+            )}
+          </div>
+
+          <div className="flex border-gray-400 border-2 items-center justify-between bg-input-bg rounded-md">
+            <input
+              className="h-12 w-full bg-input-bg px-2 rounded-md"
+              type={showConfirmPassword ? "text" : "password"}
+              id="confirm_password"
+              name="password"
+              placeholder="Confirm Password"
+            />
+            {showConfirmPassword ? (
+              <FaEyeSlash
+                onClick={toggleConfirmPasswordVisibility}
+                className="absolute right-12"
+              />
+            ) : (
+              <FaEye
+                onClick={toggleConfirmPasswordVisibility}
+                className="absolute right-12"
+              />
+            )}
+          </div>
+          <button className="bg-blue-700 text-white p-3 rounded-md w-full mt-2">
+            Sign Up
+          </button>
         </form>
 
         <span className="text-center w-full mt-4 font-[400] text-gray-500">
-          Already have an account? <Link to="/" className="text-blue-500 font-[600]">Sign In</Link>
+          Already have an account?{" "}
+          <Link to="/" className="text-blue-500 font-[600]">
+            Sign In
+          </Link>
         </span>
       </div>
     </>
