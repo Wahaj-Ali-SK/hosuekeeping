0ba31ffd8f60930dc9238fd4dba68ffdddabc1e1
Arbabhassan786 <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-28 17:03:06 +0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,16 +5,16 @@ import { Link } from "react-router-dom";
 const Navbar = ({ currentPage }) => {
   return (
     <>
-      <div className="grid grid-cols-5 border-b-2 border-black py-4 px-2 fixed w-full bg-white z-50">
+      <div className="grid grid-cols-5 border-b-2 border-black py-4 px-2 fixed w-full z-50 bg-button text-[#ffe4c4]">
         <div className="self-center">
           {currentPage === "signup" && (
             <Link to="/" className="flex items-center">
-              <IoMdArrowRoundBack size={20} />
+              <IoMdArrowRoundBack size={25} color="#ffe4c4"/>
             </Link>
           )}
           {currentPage === "settings" && (
             <Link to="/signup" className="flex items-center">
-              <IoMdArrowRoundBack size={20}/>
+              <IoMdArrowRoundBack size={25} color="#ffe4c4"/>
             </Link>
           )}
         </div>
