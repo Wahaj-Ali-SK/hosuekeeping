69ae185eff204f295ba9054c8d9d738ae53743bd
Wahaj-Ali-SK <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-05-29 18:34:11 +0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { IoIosSettings } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ currentPage }) => {
@@ -8,29 +9,36 @@ const Navbar = ({ currentPage }) => {
       <div className="grid grid-cols-5 border-b-2 border-black py-4 px-2 fixed w-full z-50 bg-button text-white">
         <div className="self-center">
           {currentPage === "signup" && (
-            <Link to="/" className="flex items-center">
-              <IoMdArrowRoundBack size={25} color="#ffe4c4"/>
+            <Link to="/login" className="flex items-center">
+              <IoMdArrowRoundBack size={25} color="#ffe4c4" />
             </Link>
           )}
           {currentPage === "settings" && (
             <Link to="/signup" className="flex items-center">
-              <IoMdArrowRoundBack size={25} color="#ffe4c4"/>
+              <IoMdArrowRoundBack size={25} color="#ffe4c4" />
             </Link>
           )}
           {currentPage === "add-details" && (
             <Link to="/" className="flex items-center">
-              <IoMdArrowRoundBack size={25} color="#ffe4c4"/>
+              <IoMdArrowRoundBack size={25} color="#ffe4c4" />
             </Link>
           )}
           {currentPage === "condo-details" && (
             <Link to="/add-details" className="flex items-center">
-              <IoMdArrowRoundBack size={25} color="#ffe4c4"/>
+              <IoMdArrowRoundBack size={25} color="#ffe4c4" />
             </Link>
           )}
         </div>
         <h1 className="justify-self-center col-span-3	text-2xl font-bold">
           Condocare
         </h1>
+        <div className="self-center flex justify-end">
+          {currentPage === "home" && (
+            <Link to="/settings" className="flex items-center">
+              <IoIosSettings size={25} color="#ffe4c4" />
+            </Link>
+          )}
+        </div>
       </div>
     </>
   );
