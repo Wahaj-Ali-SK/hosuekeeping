c1f6b19140875e78650870995f82af154fd3f926
Wahaj-Ali-SK <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-06-06 20:09:09 +0500

    Update Signin.jsx

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import google from "../../assets/icons/google.svg";
 import fb from "../../assets/icons/facebook.svg";
 import logo from '../../assets/icons/logo.png'
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../Store/UserSlicer";
 
 const Signin = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [identifier, setIdentifier] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    let userCredentails={
+      identifier, password
+    };
+    dispatch(loginUser(userCredentails)).then((result)=> {
+      if(result.payload){
+        setIdentifier('');
+        setPassword('');
+        navigate('/');
+      }
+    });
+  }
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevState) => !prevState);
@@ -14,12 +34,12 @@ const Signin = () => {
 
   return (
     <div className="flex flex-col py-2 px-8">
-      <img src={logo} alt="App logo" className="w-[90px] self-center"/>
+      <img src={logo} alt="App logo" className="w-[90px] self-center" />
       <h1 className="text-center text-2xl font-[600] mb-2">
         Sign In To Manage Your Tasks
       </h1>
       <div className="flex flex-col items-start">
-        <form className="flex flex-col w-full" action="">
+        <form className="flex flex-col w-full" onSubmit={handleLogin}>
           <div className="flex flex-col mb-4">
             <label for="email">Email:</label>
             <input
@@ -28,6 +48,8 @@ const Signin = () => {
               id="email"
               name="email"
               placeholder="Enter your email"
+              value={identifier}
+              onChange={(e) => setIdentifier(e.target.value)}
             />
           </div>
           <div className="flex flex-col mb-4">
@@ -39,20 +61,22 @@ const Signin = () => {
                 id="password"
                 name="password"
                 placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
               {showPassword ? (
-                <FaEyeSlash onClick={togglePasswordVisibility} className="absolute right-12"/>
+                <FaEyeSlash onClick={togglePasswordVisibility} className="absolute right-12" />
               ) : (
-                <FaEye onClick={togglePasswordVisibility} className="absolute right-12"/>
+                <FaEye onClick={togglePasswordVisibility} className="absolute right-12" />
               )}
             </div>
           </div>
 
           <p className="text-right mb-2 font-[600]">Forgot Password?</p>
 
-          <button className="bg-button text-white p-3 rounded-md">Log In</button>
+          <button className="bg-button text-white p-3 rounded-md" type="submit">Log In</button>
 
-          
+
         </form>
 
         <div class="flex items-center justify-center w-full">
@@ -63,11 +87,11 @@ const Signin = () => {
 
         <div className="flex flex-col w-full gap-5">
           <button className="flex bg-white text-zinc-500 p-1 items-center justify-center gap-4 border-slate-300 border-2 text-lg font-[600]">
-            <img src={google} alt="google" className="w-[35px]"/>
+            <img src={google} alt="google" className="w-[35px]" />
             Continue with Google
           </button>
           <button className="flex bg-white text-zinc-500	 p-1 items-center justify-center gap-4 border-slate-300 border-2 text-lg font-[600]">
-            <img src={fb} alt="google" className="w-[35px]"/>
+            <img src={fb} alt="google" className="w-[35px]" />
             Continue with Facebook
           </button>
         </div>
