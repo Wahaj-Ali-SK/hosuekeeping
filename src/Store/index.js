c5bf0f1e115882fb81cf409a338df990c0a5c2ac
Wahaj-Ali-SK <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-06-07 15:07:58 +0500

    Update index.js
    Update redux store with redux-persist

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from './UserSlicer';
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = configureStore({
-    reducer: {
-        user: userReducer,
-    }
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer
 });
 
-export default store;
+export const persistedStore = persistStore(store);
