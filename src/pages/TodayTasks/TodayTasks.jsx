bc559e101e64ca41d6a83bffa36166ff4d1bd6c0
Wahaj-Ali-SK <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-06-07 19:55:50 +0500

    Update TodayTasks.jsx

--- src/pages/TodayTasks/TodayTasks.jsx
+++ src/pages/TodayTasks/TodayTasks.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-import { Checkbox } from '@material-tailwind/react'
+import {
+  Checkbox,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Button,
+} from '@material-tailwind/react'
 import Footer from '../../components/Footer/Footer'
+import { useSelector } from 'react-redux';
+
+const getCurrentDayIndex = () => {
+  const currentDate = new Date();
+  return currentDate.getDay();
+};
 
 const TodayTasks = () => {
+
+  const [open, setOpen] = React.useState(false);
+  const condoTasks = useSelector(state => state.user.userTasks);
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
+  const handleOpen = () => setOpen(!open);
+
+  useEffect(() => {
+    const currentDayIndex = getCurrentDayIndex();
+    console.log(currentDayIndex, 'current date');
+
+    const filtered = condoTasks.filter(task => {
+      const { schedule } = task;
+
+      if (schedule) {
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const currentDay = daysOfWeek[currentDayIndex];
+
+        if (schedule[currentDay] !== undefined) {
+          return schedule[currentDay];
+        }
+      }
+
+      return false;
+    });
+
+    setFilteredTasks(filtered);
+  }, [condoTasks]);
+
   return (
     <>
       <Navbar currentPage="today's-tasks" />
@@ -12,63 +54,67 @@ const TodayTasks = () => {
 
         <div className='flex flex-col mt-2'>
           <h3 className='text-xl font-[600]'>Today's Tasks</h3>
-          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
-            <div className='flex flex-col'>
-              <h4 className='text-lg'>Vaccuming Carpets</h4>
-              <p className='underline text-lg'>See Notes</p>
-            </div>
-            <Checkbox defaultChecked />
-          </div>
 
-          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
-            <div className='flex flex-col'>
-              <h4 className='text-lg'>Laundry Folding</h4>
-              <p className='underline text-lg'>See Notes</p>
-            </div>
-            <Checkbox defaultChecked />
-          </div>
+          {filteredTasks.map((task, index) => (
+            <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
+              <div className='flex flex-col'>
+                <h4 className='text-lg'>{task.name}</h4>
+                <button onClick={handleOpen}><p className='underline text-lg'>See Notes</p></button>
+              </div>
+              <Checkbox checked={task.isCompleted} />
 
-          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
-            <div className='flex flex-col'>
-              <h4 className='text-lg'>Kitchen Deep Cleaning</h4>
-              <p className='underline text-lg'>See Notes</p>
+              <Dialog Dialog open={open} handler={handleOpen} >
+                <DialogHeader>Notes for the task</DialogHeader>
+                <DialogBody>
+                  {task.notes}
+                </DialogBody>
+                <DialogFooter>
+                  <Button
+                    variant="text"
+                    color="red"
+                    onClick={handleOpen}
+                    className="mr-1"
+                  >
+                    <span>Cancel</span>
+                  </Button>
+                </DialogFooter>
+              </Dialog>
             </div>
-            <Checkbox defaultChecked />
-          </div>
+          ))}
         </div>
 
         <div className='flex flex-col mt-6'>
-        <h3 className='text-xl font-[600]'>This Week's Plan</h3>
-        <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
+          <h3 className='text-xl font-[600]'>This Week's Plan</h3>
+          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
             <h4 className='text-lg'>Monday</h4>
             <p className='underline'>See Details</p>
-        </div>
-        <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
+          </div>
+          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
             <h4 className='text-lg'>Tuesday</h4>
             <p className='underline'>See Details</p>
-        </div>
-        <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
+          </div>
+          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
             <h4 className='text-lg'>Wednesday</h4>
             <p className='underline'>See Details</p>
-        </div>
-        <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
+          </div>
+          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
             <h4 className='text-lg'>Thursday</h4>
             <p className='underline'>See Details</p>
-        </div>
-        <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
+          </div>
+          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
             <h4 className='text-lg'>Friday</h4>
             <p className='underline'>See Details</p>
-        </div>
-        <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
+          </div>
+          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
             <h4 className='text-lg'>Saturday</h4>
             <p className='underline'>See Details</p>
-        </div>
-        <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
+          </div>
+          <div className='flex mt-2 p-2 justify-between border-b-2 border-black'>
             <h4 className='text-lg'>Sunday</h4>
             <p className='underline'>See Details</p>
+          </div>
         </div>
-        </div>
-      </div>
+      </div >
       <Footer />
     </>
   )
