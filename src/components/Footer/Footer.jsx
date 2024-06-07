a18eaaffa1d57aa9eba7987624e1e87e63394f17
Wahaj-Ali-SK <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-06-07 19:55:57 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,17 +3,20 @@ import { IoHomeSharp } from "react-icons/io5";
 import { FaPlusCircle } from "react-icons/fa";
 import { MdDateRange } from "react-icons/md";
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Footer = () => {
   const location = useLocation();
   const currentPath = location.pathname;
+
+  const userRole = useSelector(state=> state.user.userRole);
 
   const activeColor = '#ffe4c4';
   const defaultColor = '#d5d5d5';
   return (
     <div className="flex justify-around items-center fixed bottom-0 w-full bg-button z-50 px-2 py-4 border-t-2 border-black text-white">
       <Link to='/'><IoHomeSharp size={22} color={currentPath === '/' ? activeColor : defaultColor}/></Link>
-      <Link to="/add-task"><FaPlusCircle size={28} color={currentPath === '/add-task' ? activeColor : defaultColor}/></Link>
+      {userRole.name != 'Housekeeper' && <Link to="/add-task"><FaPlusCircle size={28} color={currentPath === '/add-task' ? activeColor : defaultColor}/></Link>}
       <Link to="/calender"><MdDateRange size={28} color={currentPath === '/calender' ? activeColor : defaultColor} /></Link>
     </div>
   );
